abdc2e266deb0011bb74652abaa7e577919a39ce
Prateek7805 <prateekpjimmy@gmail.com>
2023-03-03 00:44:48 +0530

    basic layout

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import Navbaar from './components/Navbar/Navbar';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
+import {Card} from 'react-bootstrap';
 function App() {
 
     const [navHeight, setNavHeight] = useState(20);
   return (
     <div className="App">
       <Navbaar dimension={[navHeight, setNavHeight]}/>
-      <div className='container-fluid content' style={{top: navHeight}}>
-        <p>helloasjgbaisdkjasbdjh</p>
+      <div className='container-fluid content py-2' style={{top: navHeight}}>
+        <div className='d-flex flex-column flex-md-row'>
+          <div className='d-flex flex-column col-12 col-md-6 align-items-start'>
+            <Card style={{width: '98%', height: '100px'}}>
+
+            </Card>
+          </div>  
+          <div className='d-flex flex-column col-12 col-md-6 align-items-end'>
+          <Card style={{width: '98%', height: '100px'}}>
+
+          </Card>
+          </div>
+        </div>
       </div>
     </div>
   );
